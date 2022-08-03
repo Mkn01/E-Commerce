c4c1c6d0b9e4c4183b41a498ce6d068055684d7c
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-08-03 01:24:41 +0100

    created routes for put and post request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,32 +4,103 @@ const { findAll } = require("../../models/Category");
 
 // The `/api/categories` endpoint
 
-router.get(
-  "/",
-  async (req, res) => {
+router.get("/", async (req, res) => {
+  try {
     // find all categories
-    const viewCategories = await Category.findAll;
-    if (viewCategories === null) {
+    const viewCategories = await Category.findAll({
+      include: [
+        {
+          model: Product,
+          as: "products",
+        },
+      ],
+    });
+    if (!viewCategories.length) {
       return res.status(404).json({
         error: "Not Found!",
       });
     }
+    return res.status(200).json(viewCategories);
+  } catch (err) {
+    return res.status(500).json({
+      error: "Sorry we are unable to get data for this category at this time",
+    });
   }
-
-  // be sure to include its associated Products
-);
+});
 
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [
+        {
+          model: Product,
+          as: "products",
+        },
+      ],
+    });
+    if (!category) {
+      return res.status(404).json({
+        error: "Not Found!",
+      });
+    }
+    return res.status(200).json(category);
+  } catch (err) {
+    return res.status(500).json({
+      error: "Sorry we are unable to get data for this category at this time",
+    });
+  }
+  
 });
 
 router.post("/", (req, res) => {
   // create a new category
+  try{
+    const{newCategory} = req.body
+
+    if (!newCategory) {
+      return res.status(400).json({
+        error:"values undefined",
+        message: "Please provide a new category name"
+      })
+    }
+    return res.status(200).json({
+      message:"A new category has been created",
+      category: newCategory,
+    })
+  }  catch (error) {
+    return res.status(500).json({
+      error:"Sorry we are unable to create a new category at this time"
+    })
+  }
+  
 });
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  try {
+    const {newCategory} = req.body
+    const {id} = req.params
+
+    if (!newCategory) {
+      return res.status (400).json({
+        error:"Values undefined",
+        message:"Please provide the new category name and id of the category that needs updating"
+      })
+    }
+const updateResult = await Category.update({newCategory},{
+  where: {id},
+  })
+
+if (updateResult[0] == 0) {
+  return res.status(404).json({
+    error:"Category does not exist "
+  })
+};
+return res.status(200).json({
+  error:"sorry we could not update this category at this time"
+});
+  }
 });
 
 router.delete("/:id", (req, res) => {
