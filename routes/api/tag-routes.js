2fc2cd922623614869209474efecd903a0549c67
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-08-03 16:43:40 +0100

    created get route for tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,31 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try {
+    const allTags = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+          as: "products",
+          through: ProductTag,
+        },
+      ],
+    });
+    if (!allTags.length) {
+      return res.status(404).json({
+        error: "There is no tags at the moment.",
+      });
+    }
+
+    return res.status(200).json(allTags);
+  } catch (error) {
+    return res.status(500).json({
+      error:
+        "Sorry we couldn't get all tag information at this time. Please try again later.",
+    });
+  }
 });
 
 router.get("/:id", (req, res) => {
